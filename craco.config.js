a083b18e29139c0560dddc0b28c9f195ca05b723
alexzandriabanessa <117019871+alexzandriabanessa@users.noreply.github.com>
2023-07-26 12:52:14 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -16,6 +16,6 @@ module.exports = {
               .toString().trim() : 'DEV',
  BUILD_DATE: fs.existsSync('.git') ? require('child_process')
  .execSync('git rev-parse HEAD', { cwd: __dirname })
-              .toString().trim() : 'DEV',
- BUILD_DATE: fs.existsSync('.git') ? require('child_process')
- .execSync('git show -s --format=%ci', { cwd: __dirname })
+                       .toString().trim() : new Date().toLocaleDateString(),
+        }
+      }
