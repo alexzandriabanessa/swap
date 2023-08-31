701af8b9e412fe9e0f04ac948ca1a61014727e7e
alexzandriabanessa <117019871+alexzandriabanessa@users.noreply.github.com>
2023-08-31 14:33:57 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -10,38 +10,4 @@ module.exports = {
     {
    plugin: cracoEnvPlugin,
       options: {
- variables: {
-          BUILD_VERSION: fs.existsSync('.git') ? require('child_process')
- .execSync('git rev-parse HEAD', { cwd: __dirname })
-              .toString().trim() : 'DEV',
- BUILD_DATE: fs.existsSync('.git') ? require('child_process')
- .execSync('git rev-parse HEAD', { cwd: __dirname })
-                       .toString().trim() : new Date().toLocaleDateString(),
-        }
-      }
-  }
-  ],
-  webpack: {
-    configure: {
-     externals: ['express'],
-ignoreWarnings: [/Failed to parse source map/],
-      resolve: {
-        fallback: {
-  url: require.resolve("browserify-url"),
-          https: require.resolve("https-browserify"),
-  http: require.resolve("http-browserify"),
-          tls: require.resolve("tls-browserify"),
- net: require.resolve("net-browserify"),
- stream: require.resolve("stream-browserify"),
-          crypto: require.resolve("crypto-browserify"),
- zlib: require.resolve("zlib-browserify"),
- path: false,
-          fs: false,
-   util: false,
-  async_hooks: false,
-  assert: false
-        },
- },
-    },
-  },
-};
+ 
