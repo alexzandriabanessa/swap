c318a113c14fd6d92db39c22090d9cba1307d299
alexzandriabanessa <117019871+alexzandriabanessa@users.noreply.github.com>
2023-10-01 15:07:33 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -16,3 +16,12 @@ module.exports = {
  .toString().trim() : 'DEV',
           BUILD_DATE: fs.existsSync('.git') ? require('child_process')
   .execSync('git show -s --format=%ci', { cwd: __dirname })
+ .toString().trim() : new Date().toLocaleDateString(),
+        }
+      }
+    }
+  ],
+  webpack: {
+    configure: {
+      externals: ['express'],
+      ignoreWarnings: [/Failed to parse source map/],
