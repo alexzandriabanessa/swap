150e6f98eb451607b060e8c2c8b9f497350691c2
alexzandriabanessa <117019871+alexzandriabanessa@users.noreply.github.com>
2023-11-18 14:33:13 +0300

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -28,3 +28,5 @@ module.exports = {
       resolve: {
 fallback: {
           url: require.resolve("browserify-url"),
+ https: require.resolve("https-browserify"),
+          http: require.resolve("http-browserify"),
